189def3cbc7c34ba3990f829e4a048f1ca3918be
albyabhi <alby4645@gmail.com>
2023-03-13 05:41:28 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// object
+
+// const multi =(a,b)=>{
+//     var c = a*b;
+//     console.log(c);
+
+// }
+// multi(12,12);
+// var x=10;
+// if(x<20){
+    // console.log('good');
+// }
+// else
+// // console.log("bad");
+//   for(i=1;i<=5;i++)
+//   {
+//     console.log(i);
+//   }
+fruits=["apple","orange","banana","berry"];
+let i=0;
+do{
+    console.log(fruits[i]);
+    i++
+}while(i<fruits.length)
